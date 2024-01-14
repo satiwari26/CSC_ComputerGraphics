31add57cba70bc11676f11ec9089a9895ecf526e
Saumitra Tiwari <satiwari@calpoly.edu>
2024-01-13 17:56:01 -0800

    finished lab2

--- Lab01/src/main.cpp
+++ Lab01/src/main.cpp
@@ -10,10 +10,16 @@
 // You should never do this in a header file.
 using namespace std;
 
+struct vertexColor{
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+};
+
 int main(int argc, char **argv)
 {
 	if(argc < 9) {
-		cout << "Usage: Lab1 <out_image_name>.png width height" << endl;
+		cout << "Usage: Lab2 <out_image_name>.png width height" << endl;
 		return 0;
 	}
 	// Output filename
@@ -56,22 +62,46 @@ int main(int argc, char **argv)
 	maxY = max(V1y,V2y);
 	maxY = max(maxY,V3y);
 
+	//vertex colors
+	vertexColor v1,v2,v3;
+	v1.b = 255;
+	v1.g = 0;
+	v1.r = 0;
+
+	v2.b = 0;
+	v2.g = 255;
+	v2.r = 0;
+
+	v3.b = 0;
+	v3.g = 0;
+	v3.r = 255;
+
+	//set the vertex points
+	image->setPixel(V1x, V1y, v1.r,v1.g,v1.b);
+	image->setPixel(V2x, V2y, v2.r,v2.g,v2.b);
+	image->setPixel(V3x, V3y, v3.r,v3.g,v3.b);
+
 	// Draw a rectangle
 	for(int y = minY; y <= maxY; ++y) {
 		for(int x = minX; x <= maxX; ++x) {
-			unsigned char r = 5;
-			unsigned char g = 40;
-			unsigned char b = 80;
-			image->setPixel(x, y, r, g, b);
+
+			//for each point we need to check if it lies inside the traingle or outside:
+			float gamma = float((V2x-V1x)*(y-V1y) - (x-V1x)*(V2y-V1y))/float((V2x-V1x)*(V3y-V1y) - (V3x-V1x)*(V2y - V1y));
+			float beta = float((V1x-V3x)*(y-V3y) - (x-V3x)*(V1y-V3y))/float((V2x-V1x)*(V3y-V1y) - (V3x-V1x)*(V2y - V1y));
+			float alpha = 1.0 - beta - gamma;
+
+			unsigned char r = v1.r*alpha + v2.r*beta + v3.r*gamma;
+			unsigned char g = v1.g*alpha + v2.g*beta + v3.g*gamma;
+			unsigned char b = v1.b*alpha + v2.b*beta + v3.b*gamma;
+
+			if(alpha > 0 && alpha < 1 && gamma > 0 && gamma < 1 && beta > 0 && beta < 1) {
+				image->setPixel(x, y, r, g, b);
+			}
 		}
 	}
-
-	//set the vertex points
-	image->setPixel(V1x, V1y, 90, 30, 40);
-	image->setPixel(V2x, V2y, 90, 30, 40);
-	image->setPixel(V3x, V3y, 90, 30, 40);
 
 	// Write image to file
 	image->writeToFile(filename);
 	return 0;
 }
+
