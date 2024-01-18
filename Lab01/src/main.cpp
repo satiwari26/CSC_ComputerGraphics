b933f02ec65e34d093d1df433c4462ff369e9f68
Saumitra Tiwari <satiwari@calpoly.edu>
2024-01-18 14:34:39 -0800

    made changes to the end pixel

--- Lab01/src/main.cpp
+++ Lab01/src/main.cpp
@@ -65,21 +65,21 @@ int main(int argc, char **argv)
 	//vertex colors
 	vertexColor v1,v2,v3;
 	v1.b = 255;
-	v1.g = 0;
-	v1.r = 0;
+	v1.g = 35;
+	v1.r = 23;
 
-	v2.b = 0;
+	v2.b = 32;
 	v2.g = 255;
-	v2.r = 0;
+	v2.r = 40;
 
-	v3.b = 0;
-	v3.g = 0;
+	v3.b = 78;
+	v3.g = 21;
 	v3.r = 255;
 
 	//set the vertex points
-	image->setPixel(V1x, V1y, v1.r,v1.g,v1.b);
-	image->setPixel(V2x, V2y, v2.r,v2.g,v2.b);
-	image->setPixel(V3x, V3y, v3.r,v3.g,v3.b);
+	image->setPixel(V1x, V1y, 255,255,255);
+	image->setPixel(V2x, V2y, 255,255,255);
+	image->setPixel(V3x, V3y, 255,255,255);
 
 	// Draw a rectangle
 	for(int y = minY; y <= maxY; ++y) {
@@ -88,13 +88,13 @@ int main(int argc, char **argv)
 			//for each point we need to check if it lies inside the traingle or outside:
 			float gamma = float((V2x-V1x)*(y-V1y) - (x-V1x)*(V2y-V1y))/float((V2x-V1x)*(V3y-V1y) - (V3x-V1x)*(V2y - V1y));
 			float beta = float((V1x-V3x)*(y-V3y) - (x-V3x)*(V1y-V3y))/float((V2x-V1x)*(V3y-V1y) - (V3x-V1x)*(V2y - V1y));
-			float alpha = 1.0 - beta - gamma;
+			float alpha = (1.0 - beta - gamma);
 
 			unsigned char r = v1.r*alpha + v2.r*beta + v3.r*gamma;
 			unsigned char g = v1.g*alpha + v2.g*beta + v3.g*gamma;
 			unsigned char b = v1.b*alpha + v2.b*beta + v3.b*gamma;
 
-			if(alpha > 0 && alpha < 1 && gamma > 0 && gamma < 1 && beta > 0 && beta < 1) {
+			if(alpha > -0.001 && alpha < 1 && gamma > -0.001 && gamma < 1 && beta > -0.001 && beta < 1) {
 				image->setPixel(x, y, r, g, b);
 			}
 		}
